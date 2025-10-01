2955584175bfa3e9e4279a5033d32892fa12bca5
ADMINISTRATOR <ray09273@gmail.com>
2025-10-01 19:14:03 +0800

    1001更新

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // 👇 將預設字體 (sans) 直接改為思源宋體
+        sans: [
+          '"Noto Serif TC"', // 將 'Noto Serif TC' 設為第一順位
+          ...defaultTheme.fontFamily.sans, // 保留備用字體
+        ],
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['light','dark','cupcake','retro','emerald','corporate']
+    themes: ['light', 'dark', 'cupcake', 'retro', 'emerald', 'corporate']
   }
 }
